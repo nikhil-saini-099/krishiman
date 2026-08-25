ac08388bf24a5217bec9bdc241440cdae2e2b098
nikhil-saini-099 <sd1985nikhilsaini@gmail.com>
2026-08-25 12:58:05 +0530

    Ignore ESP-IDF build files

---
+++ components/spi_drivers/include/spi_driver.h
@@ -0,0 +1,13 @@
+#ifndef SPI_DRIVER_H
+#define SPI_DRIVER_H
+
+#include <stdint.h>
+
+typedef struct {
+
+};
+void spi_init(void);
+uint8_t spi_transfer(uint8_t data);
+
+
+#endif
---
+++ main/main.c
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void)
+{
+  printf("hello") ;
+    
+   
+    // return 0;
+}
